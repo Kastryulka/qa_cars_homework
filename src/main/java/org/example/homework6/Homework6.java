09ee66fff11c1674519513c2f809575525e90ef4
Kastryulka <Kastryulka@DESKTOP-C7DSMP7>
2025-03-31 23:28:02 +0300

    третье задание шестой домашки

--- src/main/java/org/example/homework6/Homework6.java
+++ src/main/java/org/example/homework6/Homework6.java
@@ -2,12 +2,14 @@ package org.example.homework6;
 
 import org.example.homework6.iterators.tsk1.Task1;
 import org.example.homework6.iterators.tsk2.Task2;
+import org.example.homework6.iterators.tsk3.Task3;
 
 import java.io.IOException;
 import java.io.OutputStream;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class Homework6 {
@@ -20,6 +22,7 @@ public class Homework6 {
         testData.add("");
 
         List<String> result = Task1.task(testData);
+        System.out.println(result);
     }
     public static void test12() {
         String srcPath = "src/main/java/org/example/homework6/iterators/tsk2/input.txt";
@@ -40,7 +43,22 @@ public class Homework6 {
         }
         Task2.task2(srcPath, dstPath);
     }
-    public static void test13() {}
+    public static void test13() {
+        List<String> testData = new ArrayList<>();
+        testData.add("Бирюльки");
+        testData.add("Бирюльки");
+        Collections.sort(testData, new Task3());
+        System.out.println(testData);
+        testData.add("Бирюлька");
+        testData.add("Авокадо");
+        Collections.sort(testData, new Task3());
+        System.out.println(testData);
+        testData.add("");
+        testData.add(" ");
+        testData.add("123");
+        Collections.sort(testData, new Task3());
+        System.out.println(testData);
+    }
     public static void test21() {}
     public static void test22() {}
     public static void test23() {}
--- src/main/java/org/example/homework6/iterators/tsk3/Task3.java
+++ src/main/java/org/example/homework6/iterators/tsk3/Task3.java
@@ -1,8 +1,21 @@
 package org.example.homework6.iterators.tsk3;
 
+import java.util.Comparator;
+
 /**
  * Задача 3 (сложная):
  * Создайте Comparator, который сортирует строки по длине, а при равной длине – лексикографически.
  */
-public class Task3 {
+public class Task3 implements Comparator<String>{
+    public int compare(String o1, String o2) {
+        int len1 = o1.length(), len2 = o2.length();
+        if (len1 == len2) {
+            for (int i = 0; i<len2; i++){
+                if (o1.charAt(i) != o2.charAt(i)) {
+                    return o1.charAt(i) - o2.charAt(i);
+                }
+            }
+            return 0;
+        }else {return len1 - len2;}
+    }
 }
